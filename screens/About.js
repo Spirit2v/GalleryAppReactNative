7ee38b0a740d43bf21b3d22ab03e867744d677ef
adiruv <beingsuyashar@gmail.com>
2021-06-18 18:57:54 +0530

    final

--- screens/About.js
+++ screens/About.js
@@ -4,9 +4,13 @@ import { View,Button, Text } from 'react-native'
 export default function About({ navigation }) {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        
-    <Text>This is gallery app</Text>
-        <Button onPress={() => navigation.navigate('Home')} title="Go Back to Home" />
+        <View style={{padding:30,}}>
+           <Text style={{fontSize:20,fontWeight:'bold'}}>This is gallery app
+      made  by using React Native and the api used for fetching images is from flickr
+    </Text>
+   </View>
+        <Button color="coral"
+        onPress={() => navigation.navigate('Home')} title="Go Back to Home" />
       </View>
     );
   }
